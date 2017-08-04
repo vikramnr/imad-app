32fa90da245c479d26f563c5a12e15ef1dd5346a
vikramnr <nrvikram19@gmail.com>
2017-08-04 10:29:28 +0530

    Added article changes

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/first-artice',function (req,res) {
+  res.sendFile(path.join(__dirname,'ui','Hello and welcome'))
+})
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
