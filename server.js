95caf5437ea78b1d5d9130db92b64a6c19643a1f
vikramnr <nrvikram19@gmail.com>
2017-08-04 10:33:57 +0530

    Added missing First-articles

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/ui/style.css', function (req, res) {
 
 app.get('/ui/first-artice',function (req,res) {
   res.sendFile(path.join(__dirname,'ui','Hello and welcome'))
-})
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
@@ -25,7 +25,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8081;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
