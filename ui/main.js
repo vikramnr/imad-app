83066092054ca32701c6602ff1e21b81f222c3cb
Vikram <nrvikram19@gmail.com>
2017-08-05 17:46:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,11 @@ console.log('Loaded!');
 var element= document.getElementById('madi');
 
 element.onclick=function(){
-    var interval=setInterval(moveLeft,100);
+    var interval=setInterval(moveLeft,10);
     
 }
 function moveLeft(){
     ginga=0;
-    ginga=ginga+10;
+    ginga=ginga+1;
     element.style.marginLeft=ginga+'px';
 }
